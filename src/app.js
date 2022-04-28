1f363c23568c06732bc2ba654358eee8388c37bc
JOAO-VITOR-SOARES <jogalijando@gmail.com>
2022-04-28 14:22:03 -0300

    Atualização

--- src/app.js
+++ src/app.js
@@ -8,21 +8,42 @@ const port = 4001
 server.use(json());
 server.use(cors());
 
+let contador = 0
 let user = [];
 
 server.get('/', (req, res) => {
-    res.status(200).send(`${user}\n`)
+    res.status(200).send(user)
 })
 
 server.post('/', (req, res) => {
     const request = req.body
-    console.log(request.user)
+    const user2 = {
+        id: contador += 1,
+        user: request.user
+    }
+    user.push(user2)
     res.status(201).send()
 })
 
 server.delete('/', (req, res) => {
     user = [];
     res.status(200).send()
+    contador = 0
+})
+
+server.put('/:value', (req, res) => {
+    const value = req.params.value
+    const id = req.query.id
+    console.log(`QUERY IS ${id} AND PARAMETER IS ${value}`)
+
+    user.map(user => {
+        if (user.id == id){
+            console.log(`FOUND ID ${id} CHANGING VALUE OF OBJECT`)
+            user.user = value
+        }
+    });
+
+    res.send(200).send()
 })
 
 server.listen(port, (req, res)=>{

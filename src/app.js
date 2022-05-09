11b1c3b0bb817c1584c25206ba95631d18915fa5
JOAO-VITOR-SOARES <jogalijando@gmail.com>
2022-05-09 14:40:50 -0300

    database

--- src/app.js
+++ src/app.js
@@ -1,41 +1,35 @@
+
+  
 const express = require('express')
+const mongoose = require('mongoose')
 const { json } = require('body-parser')
 const cors = require('cors')
+const path = require('path');
 
-const users = require('./paginas/Routes/routes')
+const temperatures = require('./routes/temperature')
 
 const server = express()
 const port = 4001
 
-server.use(json());
-server.use(cors());
-server.use('/user', users);
+server.use(json())
+server.use(cors())
+server.use('/temperature', temperatures)
+server.use(express.static('public'))
 
-server.get('/',( req, res) => {
-    res.status(200).send(
-
-        `<html>
-        <head></head>
-        <body>
-            <h1> This is a Server Side Render Web page</h1>
-            <div>
-                <p> Ok, I'm running on port ${port}.
-            </div>
-            <h2>Server RESTful Endpoints:</h2>
-            <ul>
-                <li>curl -X POST http://localhost:4001/USUARIOS -H 'Content-Type: application/json' -d "{\"USUARIOS\": {value}}"</li>
-                <li>curl -X GET http://localhost:4001/usuarios</li>
-                <li>curl -X GET http://localhost:4001/USERS/{id}</li>
-                <li>curl -X PUT http://localhost:4001/users/{users}?id={id}</li>
-                <li>curl -X DELETE http://localhost:4001/USER</li>
-                <li>curl -X DELETE http://localhost:4001/user/query?id={id}</li>
-            </ul>
-        </body>
-    </html>
-    `
-    )
+server.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "views", "index.html"))
 })
 
-server.listen(port, (req, res)=>{
-    console.log(`Server rodando na porta: ${port}`)
+server.use((req, res) => res.status(404).sendFile(path.join(__dirname, "views", "404.html")));
+
+const main = async () => {
+  await mongoose.connect(`mongodb+srv://JoaoVitor:JoaoVitor@cluster0.xeu75.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+}
+
+main()
+.then(() => {
+    server.listen(port, () => {
+        console.log(`server running on port ${port}`)
+      })
 })
+.catch(err => console.log(err));
